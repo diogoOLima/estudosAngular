88345282ad160d19cc8d49810fb42b86e84d9e2b
Diogo Oliveira de Lima <diogo.oliveira.lima1@gmail.com>
2023-01-19 06:34:27 -0300

    api rest

--- bytebank/src/app/extrato/extrato.component.ts
+++ bytebank/src/app/extrato/extrato.component.ts
@@ -1,3 +1,4 @@
+import { Transferencia } from './../models/transferencia.model';
 import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Input } from '@angular/core';
 
@@ -13,6 +14,9 @@ export class ExtratoComponent {
   }
 
   ngOnInit(){
-    this.transferencias = this.service.transferencias;
+    this.service.todas().subscribe((transferencias: Transferencia[]) => {
+      console.table(transferencias);
+      this.transferencias = transferencias;
+    })
   }
 }
--- bytebank/src/app/services/transferencia.service.ts
+++ bytebank/src/app/services/transferencia.service.ts
@@ -1,16 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Transferencia } from '../models/transferencia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
-  constructor() {
+  private url: 'http://localhost:3000/transferencias';
+
+  constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
   get transferencias(){
     return this.listaTransferencia;
+  }
+
+  todas(): Observable<Transferencia[]>{
+    return this.httpClient.get<Transferencia[]>(this.url);
   }
 
   adicionar(transferencia: any){
